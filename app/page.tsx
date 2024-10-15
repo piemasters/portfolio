7e96faf109892429f7c9a25a86cce1a53202b1ac
David Norton <pie_masters@hotmail.co.uk>
2024-10-15 23:43:40 +0100

    feat: new intro page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Intro from "@/components/intro";
+
 export default function Home() {
   return (
     <section className="py-24">
       <div className="container max-w-3xl">
-        <h1 className="text-3xl font-bold">Portfolio</h1>
+        <Intro />
       </div>
     </section>
   );
