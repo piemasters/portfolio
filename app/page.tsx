9fcb581e027235706f00f8d79357324ca791ab41
David Norton <pie_masters@hotmail.co.uk>
2024-11-04 14:12:16 +0000

    feat: generate posts using gray-matter, next-mdx-remote and sugar-high

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import { MDXRemote } from "next-mdx-remote/rsc";
+
 import Intro from "@/components/intro";
 
 export default function Home() {
+  const content = `# This is a markdown heading`;
+
   return (
     <section className="py-24">
       <div className="container max-w-3xl">
         <Intro />
+        <MDXRemote source={content} />
       </div>
     </section>
   );
