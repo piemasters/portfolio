da811de5c615164c5c015cf9c124893998d497ce
David Norton <pie_masters@hotmail.co.uk>
2024-11-04 18:32:29 +0000

    feat: add recent posts component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
-import { MDXRemote } from "next-mdx-remote/rsc";
-
 import Intro from "@/components/intro";
+import RecentPosts from "@/components/recent-posts";
 
 export default function Home() {
-  const content = `# This is a markdown heading`;
-
   return (
     <section className="py-24">
       <div className="container max-w-3xl">
         <Intro />
-        <MDXRemote source={content} />
+        <RecentPosts />
       </div>
     </section>
   );
